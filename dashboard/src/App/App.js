e82f4938508f6aa1f747d92017ceb4d009533705
esayyas ahmed <esayyasahmed1012@gmail.com>
2024-04-17 21:02:58 -0700

    .\App.css

--- dashboard/src/App/App.js
+++ dashboard/src/App/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import "./App.css";
-import logo from '../assets/newlogo.png'
-
+import Header from '../Header/Header';
 const App = () => {
 	return (
 		<div className="App">
-			<img src={logo} className ="logo_img" alt="logo" />
-			<h1>Esayyas Ahmed</h1>
+			<Header />
 		</div>
 	)
 }
