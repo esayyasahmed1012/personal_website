7fae5a9678f54caad4b1219fad726a74dc42390e
esayyas ahmed <esayyasahmed1012@gmail.com>
2024-04-17 21:03:36 -0700

    .\Header.css

---
+++ dashboard/src/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css"
+const Header = () => {
+    return ( 
+        <div className="Header">
+            <button className="">Esayyas Ahmed</button>
+			<ul>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">Experience</a></li>
+                <li><a href="#">Learning</a></li>
+                <li><a href="#">Qoute</a></li>
+                <li><a href="#">Contacts</a></li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default Header;
