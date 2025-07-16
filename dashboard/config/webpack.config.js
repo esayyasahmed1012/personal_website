20a7388528f79ecf459a4f4d3df325833ef1884a
esayyas ahmed <80974797+esayyasahmed1012@users.noreply.github.com>
2025-07-16 10:33:42 +0300

    Update webpack.config.js

--- dashboard/config/webpack.config.js
+++ dashboard/config/webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
 	entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-		path: path.resolve('./dist')
+	path: path.resolve('./dist'),
+	publicPath: '/'
     },
     module:{
         rules:[
