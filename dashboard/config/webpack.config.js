c7f9172ef592cb7c5fc46be94032f79e98896274
esayyasahmed1012 <esayyasahmed1012@gmail.com>
2025-07-16 20:35:59 -0700

    all

--- dashboard/config/webpack.config.js
+++ dashboard/config/webpack.config.js
@@ -1,53 +1,51 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    mode: 'development',
-	entry: './src/index.js',
+    mode: process.env.NODE_ENV || 'development', // Use environment variable for mode
+    entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-	path: path.resolve('./dist'),
-	publicPath: '/'
+        path: path.resolve(__dirname, '../dist'), // Adjusted to ensure dist is in root
+        publicPath: '/',
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.css/,
-                use:['style-loader', 'css-loader']
-    
-            }, 
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
             {
-                test:/\.(png|jpe?g|gif)$/i,
-                use:[
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
                     'file-loader',
                     {
-                        loader:'image-webapck-loader',
-                        options:{
+                        loader: 'image-webpack-loader', // Fixed typo
+                        options: {
                             bypassOnDebug: true,
-                            disable: true
-                        }
-                    }
-                ]
+                            disable: true,
+                        },
+                    },
+                ],
             },
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use:{
-                    loader:'babel-loader',
-                    options:{
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            }
-        ]
-        
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+        ],
     },
     resolve: {
-        extensions: ['.js', '.jsx']
-      },
-    devtool: 'inline-source-map',
-	devServer: {
-		static: path.resolve('./dist'),
-		compress: true,
-		port: 8564,
-	},
-}
+        extensions: ['.js', '.jsx'],
+    },
+    devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'inline-source-map', // Optimize for production
+    devServer: {
+        static: path.resolve(__dirname, '../dist'),
+        compress: true,
+        port: 8564,
+    },
+};
